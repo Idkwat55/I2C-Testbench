d3d9c4b12d84ab9b0f98d3251c31cdb2ae3af5c9
IdkWat55 <117009120+Idkwat55@users.noreply.github.com>
2025-02-02 18:49:15 +0530

    misc

--- verilator.f
+++
@@ -1,1 +0,0 @@
-i2c_master.v i2c_slave.v stream_gen.v muxDataGen_r.v muxDataGen_w.v manual_tb.sv stream_read.v --top manual_tb --timing -Wno-WIDTHEXPAND -Wno-WIDTHTRUNC -Wno-CASEINCOMPLETE -j 0 --trace-fst
